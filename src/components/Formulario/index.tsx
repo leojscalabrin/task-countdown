316e909ad942dd791fa712288085b5d221a65e11
Leonardo Scalabrin <leojscalabrin@gmail.com>
2022-02-24 21:46:27 -0300

    aula props

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -8,13 +8,29 @@ class Formulario extends React.Component {
       <form className={style.novaTarefa}>
         <div className={style.inputContainer}>
           <label htmlFor="tarefa">Adicione um novo estudo</label>
-          <input type="text" name="tarefa" id="tarefa" placeholder="O que você quer estudar" required/>
+          <input
+            type="text"
+            name="tarefa"
+            id="tarefa"
+            placeholder="O que você quer estudar"
+            required
+          />
         </div>
         <div className={style.inputContainer}>
           <label htmlFor="tempo">Tempo</label>
-          <input type="time" step="1" name="tempo" id="tempo" min="00:00:00" max="01:30:00" required />
+          <input
+            type="time"
+            step="1"
+            name="tempo"
+            id="tempo"
+            min="00:00:00"
+            max="01:30:00"
+            required
+          />
         </div>
-        <Botao />
+        <Botao>
+          Adicionar
+        </Botao> 
       </form>
     );
   }
