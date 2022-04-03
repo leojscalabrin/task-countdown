0e6623dc641958eb2933493545315e7b86121a78
Leonardo Scalabrin <leojscalabrin@gmail.com>
2022-04-03 19:47:39 -0300

    finalizando e refatorando

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './pages/App';
 
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import Cronometro from '../components/Cronometro';
 import Formulario from '../components/Formulario';
-import Lista from "../components/Lista";
-import { ITarefa } from '../types/tarefas';
-import style from "./App.module.scss";
+import Lista from '../components/Lista';
+import { ITarefa } from '../types/tarefa';
+import style from './App.module.scss';
 
 function App() {
   const [tarefas, setTarefas] = useState<ITarefa[]>([]);
@@ -14,17 +14,36 @@ function App() {
     setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
       ...tarefa,
       selecionado: tarefa.id === tarefaSelecionada.id ? true : false
-    })));
+    })))
+  }
+
+  function finalizarTarefa() {
+    if(selecionado) {
+      setSelecionado(undefined);
+      setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => {
+        if(tarefa.id === selecionado.id) {
+          return {
+            ...tarefa,
+            selecionado: false,
+            completado: true
+          }
+        }
+        return tarefa;
+      }))
+    }
   }
 
   return (
     <div className={style.AppStyle}>
       <Formulario setTarefas={setTarefas} />
-      <Lista 
-      tarefas={tarefas}
-      selecionaTarefa={selecionaTarefa}
+      <Lista
+        tarefas={tarefas}
+        selecionaTarefa={selecionaTarefa}
       />
-      <Cronometro selecionado={selecionado}/>
+      <Cronometro
+        selecionado={selecionado}
+        finalizarTarefa={finalizarTarefa}
+      />
     </div>
   );
 }
